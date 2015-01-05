b5cb3760a2d1642301b2abf99b95506e166f794b
mvinan <Alejandro@MacMiguel.local>
2015-01-04 21:38:40 -0500

    construyéndola
    cambios en el header y en el home

--- js/js.js
+++ js/js.js
@@ -1,11 +1,14 @@
 $(document).ready(main);
 
-var contador = 1;
-
 function main(){
+	$('.Header-btMenu').click(function(){
+		$('.Header-nav').slideToggle('slow');
+	});
+
 
 	$("#slider3").responsiveSlides({
 		manualControls: '#slider3-pager',
 		maxwidth: 800
 	});
+
 }
