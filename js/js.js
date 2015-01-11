4c6b38de0c69e1b555b97c9d53eb6be7049ea8bc
mvinan <Alejandro@MacMiguel.local>
2015-01-11 13:55:43 -0500

    borrando inutilidades :)

--- js/js.js
+++ js/js.js
@@ -10,16 +10,3 @@ function inicio(){
 		maxwidth: 800
 	});
 	var $win = $(window);
-
-	// $win.scroll(function(){
-	// 	if ($win.scrollTop() == 0) {
-	// 		alert("estamos en top");
-	// 	}
-	// });
-
-
-}
-
-//window.onload=function(){
-//alert("imagenes, cargadas y listas");
-//}
