9e5d4806306db45bccaa62ac826cff60c61ebc3c
mvinan <Alejandro@MacMiguel.local>
2015-01-11 22:21:55 -0500

    avananzando :)

--- js/js.js
+++ js/js.js
@@ -32,14 +32,62 @@ function init() {
 function start() {
 	
 	$('body').removeClass("loading").addClass('loaded');
+	window.scrollTo(0,0);
 
-	jQuery(function($) {
-	$('.Home-link').waypoint(function() {
-		$(this).toggleClass( 'fadeInRightBig animated' );
-		},
-		{
-			offset: '70%',
-			triggerOnce: true
+window.setTimeout(function(){
+		//aqui esta el codigo que ejecuta las animaciones
+
+		jQuery(function($) {
+			//poniendo tiempos para el header salga en secuencia
+			window.setTimeout(function(){
+				$('.Header-item:nth-child(2)').waypoint(function(){
+					$(this).toggleClass('bounceInLeft animated');},
+					{
+						offset:'70%',
+						triggerOnce:true
+				});
+			}, 500);
+			window.setTimeout(function(){
+				$('.Header-item:nth-child(3)').waypoint(function(){
+					$(this).toggleClass('bounceInLeft animated');},
+					{
+						offset:'70%',
+						triggerOnce:true
+				});
+			}, 700);
+			window.setTimeout(function(){
+				$('.Header-item:nth-child(4)').waypoint(function(){
+					$(this).toggleClass('bounceInLeft animated');},
+					{
+						offset:'70%',
+						triggerOnce:true
+				});
+			}, 900);
+
+			//inician las nimaciones de Home
+			$('.Home-logo').waypoint(function() {
+				$(this).toggleClass( 'bounceInDown animated' );
+				},
+				{
+					offset: '50%',
+					triggerOnce: true
+				});
+			$('.Home-link').waypoint(function(){
+				$(this).toggleClass('fadeInRightBig animated');},
+				{
+					offset:'70%',
+					triggerOnce:true
+			});
+			$('.Home-boxName').waypoint(function(){
+				$(this).toggleClass('bounceInUp animated');},
+				{
+					offset:'70%',
+					triggerOnce:true
+			});
+
 		});
-	});
+		// hasta aqui es el codigo ejecutador de animated.css
+}, 500);
+
+//Termina la funcion start
 }
